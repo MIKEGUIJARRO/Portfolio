cb41df1dd6d5888c145097aa876a4ff769ca34a0
Mike Guijarro <a01612042@itesm.mx>
2021-02-08 10:56:10 -0600

    Primer commit

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require("express"),
+    exphbs = require("express-handlebars"),
+    bodyparser = require("body-parser");
+
+const path = require("path");
+const rootDir = require("./lib/path");
+
+//Database setup
+//const mongodb = require("./database");
+
+//Initialization
+const app = express();
+
+//Settings
+app.set("port", process.env.PORT || 5000);
+console.log(app);
+app.set("views", path.join(rootDir, "views"))
+
+//Html templating setup
+app.engine(
+    ".hbs",
+    exphbs({
+        extname: ".hbs",
+        defaultLayout: "main",
+        layoutsDir: path.join(app.get("views"), "layouts"),
+        partialsDir: path.join(app.get("views"), "partials"),
+        helpers: path.join(rootDir, "lib", "helpers.js"),
+    })
+);
+
+//Html templating use
+app.set("view engine", ".hbs");
+
+//Midlewares
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+//Public
+app.use(express.static(path.join(rootDir, "public")));
+
+//Routes
+app.use(require("./routes/index"));
+
+
+module.exports = app;
