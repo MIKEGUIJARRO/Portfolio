58c6aa366d0888f66430f9e61bb60a1b6d7c3756
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-25 15:22:11 -0600

    Add <VideoPlayer />

---
+++ src/components/video-player.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { useCallback, useEffect, useRef, useState } from 'react';
+import ReactPlayer from 'react-player';
+import screenfull from 'screenfull';
+import { Maximize, Minimize } from 'lucide-react';
+
+import { Button } from './ui/button';
+
+export const VideoPlayer = () => {
+
+    const [isFullscreen, setIsFullscreen] = useState(false)
+    const [isClient, setIsClient] = useState(false);
+
+    const refPlayer = useRef<ReactPlayer>(null)
+
+    const handleScreenfullChange = useCallback(() => {
+        setIsFullscreen(screenfull.isFullscreen)
+    }, [])
+
+    useEffect(() => {
+        setIsClient(true)
+        if (screenfull.isEnabled) {
+            screenfull.on('change', handleScreenfullChange)
+        }
+        return () => {
+            if (screenfull.isEnabled) {
+                screenfull.on('change', handleScreenfullChange)
+            }
+        }
+    }, [])
+
+    const onFullScreen = useCallback(() => {
+        if (refPlayer.current === null) {
+            return
+        }
+        const videoElement = refPlayer.current.getInternalPlayer() as HTMLVideoElement;
+        if (screenfull.isEnabled) {
+            screenfull.request(videoElement)
+        }
+    }, [refPlayer.current])
+
+    return (
+        <div className='relative w-full h-full'>
+            {isClient && <ReactPlayer
+                ref={refPlayer}
+                className='absolute inset-0 [&>video]:object-cover'
+                width='100%'
+                height='100%'
+                loop={true}
+                controls={false}
+                playing={true}
+                muted={!isFullscreen}
+                config={{ file: { attributes: { controlsList: 'nodownload', disablePictureInPicture: true } } }}
+                url={'https://d29p8yz5fxctco.cloudfront.net/assets/demo-dartcom.mp4'}
+            />}
+            <Button
+                className='absolute bottom-0 right-0 m-4 rounded-full'
+                variant={'outline'}
+                size="icon"
+                onClick={onFullScreen}>
+                {isFullscreen ? <Minimize /> : <Maximize />}
+            </Button>
+        </div>
+    )
+}
