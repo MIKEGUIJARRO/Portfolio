af2d805f430eb77c6024fab688abae679442391c
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-26 13:38:26 -0600

    Remove dragging internal state from <GridItem /> and move it to parent

--- src/components/grid/item.tsx
+++ src/components/grid/item.tsx
@@ -3,47 +3,24 @@ import React, { useEffect, useState } from 'react'
 
 interface IGridItemProps extends React.HTMLAttributes<HTMLDivElement> {
     isDragging: boolean
+    isDraggingDelayed: boolean
 }
 
-export const GridItem = React.forwardRef<HTMLDivElement, IGridItemProps>(({ style, className, onMouseDown, onMouseUp, onTouchEnd, children, isDragging, ...props }, ref) => {
-
-    const [isDragged, setIsDragged] = useState<boolean>(false)
-
-    const onMouseDownHandler = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        if (onMouseDown) {
-            onMouseDown(event)
-        }
-        setIsDragged(true)
-    }
-
-    const onMouseUpHandler = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        if (onMouseUp) {
-            onMouseUp(event)
-        }
-        setIsDragged(false)
-    }
-
-    const onTouchEndHandler = (event: React.TouchEvent<HTMLDivElement>) => {
-        if (onTouchEnd) {
-            onTouchEnd(event)
-        }
-        setIsDragged(false)
-    }
-
+export const GridItem = React.forwardRef<HTMLDivElement, IGridItemProps>(({ style, className, onMouseDown, onMouseUp, onTouchEnd, children, isDragging, isDraggingDelayed, ...props }, ref) => {
 
     return (
         <div
             ref={ref}
-            onMouseDown={onMouseDownHandler}
-            onMouseUp={onMouseUpHandler}
-            onTouchEnd={onTouchEndHandler}
             style={{
                 ...style,
-                zIndex: isDragging ? 20 : 10
+                zIndex: isDraggingDelayed ? 20 : 10
             }}
+            onMouseDown={onMouseDown}
+            onMouseUp={onMouseUp}
+            onTouchEnd={onTouchEnd}
             className={cn(className)}
             {...props}>
-            <div className={`rounded-2xl overflow-hidden w-full h-full hover:cursor-grab transition-all duration-200 ${isDragged && 'shadow-2xl scale-[1.10]'}`}>
+            <div className={`w-full h-full hover:cursor-grab transition-all duration-200 ${isDragging && 'scale-[1.10]'}`}>
                 {children}
             </div>
         </div>
