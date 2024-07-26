13acfb8a313f1bbfb0a9615a5be0054a179533d1
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-26 11:21:51 -0600

    Add with-title higher order component

---
+++ src/components/cards/with-title.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { FC } from "react";
+
+interface IWithTitleProps {
+    title: string
+}
+
+export function withTitle<P extends {}>(
+    Component: React.ComponentType<P>
+): React.ComponentType<P & IWithTitleProps> {
+    const ComponentWithTitle: FC<P & IWithTitleProps> = ({ title, ...props }) => {
+        return (
+            <div className="relative w-full h-full">
+                <Component {...(props as unknown as P)} />
+                <div className="absolute bottom-0 left-0 m-4">
+                    <p className="px-4 py-2 bg-white/80 backdrop-blur-lg border border-slate-200 rounded-lg">{title}</p>
+                </div>
+            </div>
+        )
+    }
+
+    return ComponentWithTitle
+}
