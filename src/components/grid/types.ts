2966cc3904111bd05ad648ee97960e2ae42ae21e
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-26 16:08:41 -0600

    Refactor grid types to support generic data

--- src/components/grid/types.ts
+++ src/components/grid/types.ts
@@ -1,15 +1,17 @@
 import { Layout } from "react-grid-layout"
 
-export type GridItemTypes = 'square_1x1' | 'column_1x2' | 'row_2x1' | 'square_2x2' | 'rectangle_4_2'
+export type TGridItemTypes = 'square_1x1' | 'column_1x2' | 'row_2x1' | 'square_2x2' | 'rectangle_4_2'
 
-export interface IGridItem {
+export interface IGridItem<T> {
     id: string
     layout: Layout
-    type: GridItemTypes
+    type: TGridItemTypes
+    data: T
 }
 
-export interface IGridContent {
+export interface IGridContent<T> {
     id: string
-    type: GridItemTypes
+    type: TGridItemTypes
     isDragging: boolean
+    data: T
 }
