c413fc7228137a2f5a5076314e0740f5803be774
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-26 13:38:52 -0600

    Add dragg item state to <Grid />

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -5,7 +5,7 @@ import '/node_modules/react-resizable/css/styles.css'
 import './grid.css'
 
 import { FC, useMemo, useState } from 'react'
-import GridLayout, { ItemCallback, Layout, Responsive, WidthProvider } from "react-grid-layout"
+import { ItemCallback, Layout, Responsive, WidthProvider } from "react-grid-layout"
 
 import { GridItem } from './item'
 import { IGridContent, IGridItem } from './types'
@@ -19,22 +19,21 @@ interface IGridProps {
 
 export const Grid: FC<IGridProps> = ({ items, GridContent }) => {
     const [activeDraggedItem, setActiveDraggedItem] = useState<string | null>(null)
+    const [activeDraggedItemDelayed, setActiveDraggedItemDelayed] = useState<string | null>(null)
+
     const memoItemsLayout: Layout[] = useMemo(() => items.map((gridItem) => gridItem.layout), [items])
 
     /* Drag Handlers */
     const onDragStartHandler: ItemCallback = (layout, oldItem, newItem, placeholder, event, element) => {
         setActiveDraggedItem(oldItem.i)
+        setActiveDraggedItemDelayed(oldItem.i)
     }
 
     const onDragStopHandler: ItemCallback = (layout, oldItem, newItem, placeholder, event, element) => {
+        setActiveDraggedItem(null)
         setTimeout(() => {
-            setActiveDraggedItem(null)
-        }, 200);
-    }
-
-    /* Drag Handler */
-    const onDragHandler: ItemCallback = () => {
-        console.log('onDragHandler')
+            setActiveDraggedItemDelayed(null)
+        }, 200)
     }
 
     return (
@@ -48,16 +47,18 @@ export const Grid: FC<IGridProps> = ({ items, GridContent }) => {
             rowHeight={270}
             onDragStart={onDragStartHandler}
             onDragStop={onDragStopHandler}
-            onDrag={onDragHandler}
             preventCollision={false}
             isResizable={false}>
             {items.map((item) => {
+                const isDraggingDelayed = activeDraggedItemDelayed ? activeDraggedItemDelayed === item.id : false
                 const isDragging = activeDraggedItem ? activeDraggedItem === item.id : false
                 return (
                     <GridItem
                         key={item.id}
-                        isDragging={isDragging}>
-                        <GridContent id={item.id} type={item.type} />
+                        isDragging={isDragging}
+                        isDraggingDelayed={isDraggingDelayed}
+                        >
+                        <GridContent id={item.id} type={item.type} isDragging={isDragging} />
                     </GridItem>
                 )
             })}
