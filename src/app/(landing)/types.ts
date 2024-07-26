667ea9b55cca1ba69117521bb0b1a571cc1a5278
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-26 16:15:01 -0600

    Add types to landing section

---
+++ src/app/(landing)/types.ts
@@ -0,0 +1,19 @@
+import { IGridContent, IGridItem } from "@/components/grid/types"
+
+type TProjectDataCommon = {
+    title: string
+    href: string
+    src: string
+}
+
+export type TProjectData =
+    | {
+        type: 'image'
+        alt: string
+    } & TProjectDataCommon
+    | {
+        type: 'video'
+    } & TProjectDataCommon
+
+export interface IProjectGridItem extends IGridItem<TProjectData> { }
+export interface IProjectGridContent extends IGridContent<TProjectData> { }
