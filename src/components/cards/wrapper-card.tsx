4dcecaf893d001cb92ae7ca5e203daefd15e6e45
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-26 13:39:24 -0600

    Add shadow prop to <WrapperCard />

--- src/components/cards/wrapper-card.tsx
+++ src/components/cards/wrapper-card.tsx
@@ -2,11 +2,12 @@ import React, { FC, ReactNode } from 'react'
 
 interface IWrapperCardProps {
     children: ReactNode
+    shadowHidden?: boolean
 }
 
-export const WrapperCard: FC<IWrapperCardProps> = ({ children }) => {
+export const WrapperCard: FC<IWrapperCardProps> = ({ children, shadowHidden = false }) => {
     return (
-        <div className={'w-full h-full border border-slate-200 rounded-2xl overflow-hidden'}>
+        <div className={`w-full h-full border border-slate-200 rounded-2xl overflow-hidden transition-all duration-200 ${!shadowHidden && 'shadow-2xl'}`}>
             {children}
         </div>
     )
