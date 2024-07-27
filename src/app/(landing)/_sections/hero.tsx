f0e90b1d304d7340ff82e1727a7217324e20e680
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-27 02:26:07 -0600

    Refactor hero mobile support

--- src/app/(landing)/_sections/hero.tsx
+++ src/app/(landing)/_sections/hero.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function HeroSection() {
     return (
-        <div className="h-screen m-auto flex justify-center items-center gap-8">
+        <div className="h-screen m-auto flex flex-col lg:flex-row justify-center items-center gap-8">
             <div>
                 <Image src={'/profile_pic.jpeg'} alt="Profile picture" width={400} height={800} className="object-cover w-64 h-96 rounded-3xl" />
                 <div className="pt-2">
@@ -12,8 +12,8 @@ export function HeroSection() {
                     </Link>
                 </div>
             </div>
-            <div className="space-y-4 w-full max-w-lg">
-                <h1 className="text-8xl font-bold uppercase">Software Engineer</h1>
+            <div className="space-y-4 w-full max-w-lg text-center lg:text-start">
+                <h1 className="text-6xl lg:text-8xl font-bold uppercase">Software Engineer</h1>
                 <h2>Currently building <Link href={'https://dartcom.io'} target="_blank">dartcom.io</Link> in public</h2>
             </div>
         </div>
