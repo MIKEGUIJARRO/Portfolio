b2d40183f14ff559919728d974534c73c2d51453
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-27 02:20:58 -0600

    Refactor landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { CarouselLogos } from "@/components/carousel-logos";
 import { ProjectsSection } from "./(landing)/_sections/projects";
 import { HeroSection } from "./(landing)/_sections/hero";
 import { AboutMeSection } from "./(landing)/_sections/about-me";
+import { FooterSection } from "./(landing)/_sections/footer";
 
 
 export default function Home() {
@@ -9,11 +10,14 @@ export default function Home() {
     <main className="container">
       <HeroSection />
       <CarouselLogos />
-      <div className="pt-40">
+      <div className="pt-80">
         <ProjectsSection />
       </div>
-      <div className="pt-40">
+      <div className="pt-80">
         <AboutMeSection />
+      </div>
+      <div className="pt-80">
+        <FooterSection />
       </div>
     </main>
   );
