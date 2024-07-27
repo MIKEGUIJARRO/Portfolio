ee0f1ae3f5524972eb7e67af6d46bacbb9abb28b
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-26 20:03:38 -0600

    Refactor next config to support aws hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'd29p8yz5fxctco.cloudfront.net',
+            },
+        ]
+    }
+};
 
 export default nextConfig;
