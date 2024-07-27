e2bedeb5495a0ab503455982f5ce94da4d05b972
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-27 02:20:04 -0600

    Add toast and clipboard features to footer

--- src/app/(landing)/_sections/footer.tsx
+++ src/app/(landing)/_sections/footer.tsx
@@ -6,9 +6,21 @@ import { Copy } from "lucide-react";
 import Link from "next/link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faLinkedinIn, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { useToast } from "@/components/ui/use-toast";
 
 
 export function FooterSection() {
+
+    const { toast } = useToast()
+
+    const onEmailClick = () => {
+        toast({
+            title: "Email copied succesfully ✅",
+            description: "mikeguijarrodev@gmail.com copied to clipboard",
+        })
+        navigator.clipboard.writeText('mikeguijarrodev@gmail.com');
+    }
+
     return (
         <div className="py-16 flex gap-4">
             <Link
@@ -29,7 +41,10 @@ export function FooterSection() {
                 className={cn(buttonVariants({ variant: 'outline', size: 'icon' }), 'rounded-full transition-all')}>
                 <FontAwesomeIcon icon={faInstagram} size="lg" />
             </Link>
-            <Button variant={'outline'} className="space-x-2">
+            <Button
+                variant={'outline'}
+                className="space-x-2"
+                onClick={onEmailClick}>
                 <Copy />
                 <span>mikeguijarrodev@gmail.com</span>
             </Button>
