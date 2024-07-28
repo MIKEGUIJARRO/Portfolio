8410b1f902d1aba963262990f36fc1670f3f0e3d
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-28 14:19:32 -0600

    Add <TooltipCard /> to <GridContentWrapper />

--- src/app/(landing)/_components/grid-content-wrapper.tsx
+++ src/app/(landing)/_components/grid-content-wrapper.tsx
@@ -1,17 +1,20 @@
 'use client'
 
 import { FC } from "react"
-import { WrapperCard } from "../../../components/cards/wrapper-card"
 import { GridContent } from "./grid-content"
 import { IProjectGridContent } from "../types"
 import { AnimationCard } from "@/components/cards/animation-card"
+import { TooltipCard } from "@/components/cards/tooltip-card"
+import { WrapperCard } from "../../../components/cards/wrapper-card"
 
 export const GridContentWrapper: FC<IProjectGridContent> = ({ id, type, isDragging, data }) => {
     return (
         <AnimationCard>
-            <WrapperCard shadowHidden={!isDragging}>
-                <GridContent id={id} type={type} data={data} />
-            </WrapperCard>
+            <TooltipCard>
+                <WrapperCard shadowHidden={!isDragging}>
+                    <GridContent id={id} type={type} data={data} />
+                </WrapperCard>
+            </TooltipCard>
         </AnimationCard>
     )
 }
