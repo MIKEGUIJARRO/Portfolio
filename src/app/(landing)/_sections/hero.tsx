409c73be13b52f1d59e6467fec56cc84bba6e11e
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-28 15:43:39 -0600

    Add animation to hero image and twitter @

--- src/app/(landing)/_sections/hero.tsx
+++ src/app/(landing)/_sections/hero.tsx
@@ -7,7 +7,7 @@ import { motion, Variants } from "framer-motion";
 const itemVariants: Variants = {
     hidden: {
         opacity: 0,
-        y: 10
+        y: 20
     },
     visible: {
         opacity: 1,
@@ -18,18 +18,23 @@ const itemVariants: Variants = {
 export function HeroSection() {
     return (
         <div className="h-screen m-auto flex flex-col lg:flex-row justify-center items-center gap-8">
-            <div>
-                <Image src={'/profile_pic.jpeg'} alt="Profile picture" width={400} height={800} className="object-cover w-64 h-96 rounded-3xl" />
-                <div className="pt-2">
-                    <Link href={'https://x.com/mikeguijarro'} target="_blank">
-                        @mike_guijarro
-                    </Link>
-                </div>
-            </div>
             <motion.div
                 initial="hidden"
                 animate="visible"
-                variants={{ visible: { transition: { staggerChildren: 0.6 } } }}
+                variants={{ visible: { transition: { staggerChildren: 2 } } }}>
+                <motion.div variants={itemVariants}>
+                    <Image src={'/profile_pic.jpeg'} alt="Profile picture" width={400} height={800} className="object-cover w-64 h-96 rounded-3xl" />
+                </motion.div>
+                <motion.div variants={itemVariants} className="pt-2">
+                    <Link href={'https://x.com/mikeguijarro'} target="_blank">
+                        @mike_guijarro
+                    </Link>
+                </motion.div>
+            </motion.div>
+            <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={{ visible: { transition: { staggerChildren: 1 } } }}
                 className="space-y-4 w-full max-w-lg text-center lg:text-start">
                 <motion.h1
                     variants={itemVariants}
