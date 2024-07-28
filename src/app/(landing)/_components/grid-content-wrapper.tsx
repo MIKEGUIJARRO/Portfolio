9e37062ab128bb15ac4c0311b486ac7a74c61089
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-28 13:39:34 -0600

    Add <AnimationCard /> to <GridContentWrapper />

--- src/app/(landing)/_components/grid-content-wrapper.tsx
+++ src/app/(landing)/_components/grid-content-wrapper.tsx
@@ -4,11 +4,14 @@ import { FC } from "react"
 import { WrapperCard } from "../../../components/cards/wrapper-card"
 import { GridContent } from "./grid-content"
 import { IProjectGridContent } from "../types"
+import { AnimationCard } from "@/components/cards/animation-card"
 
 export const GridContentWrapper: FC<IProjectGridContent> = ({ id, type, isDragging, data }) => {
     return (
-        <WrapperCard shadowHidden={!isDragging}>
-            <GridContent id={id} type={type} data={data} />
-        </WrapperCard>
+        <AnimationCard>
+            <WrapperCard shadowHidden={!isDragging}>
+                <GridContent id={id} type={type} data={data} />
+            </WrapperCard>
+        </AnimationCard>
     )
 }
