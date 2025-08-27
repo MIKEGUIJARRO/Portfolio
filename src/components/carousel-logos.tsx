b5b1d631af8d601f0aefac6310d08a2b57b1e8d9
Mike Guijarro <mikeguijarrodev@gmail.com>
2025-08-26 19:23:30 -0600

    Add AWS logo

--- src/components/carousel-logos.tsx
+++ src/components/carousel-logos.tsx
@@ -7,14 +7,14 @@ import {
     Carousel,
     CarouselContent,
     CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
 } from "@/components/ui/carousel"
 
 type logo = {
     key: string
     src: string
     alt: string
+    width?: number
+    height?: number
 }
 
 const logosArr: logo[] = [
@@ -22,24 +22,41 @@ const logosArr: logo[] = [
         key: 'dartcom',
         src: '/dartcom.svg',
         alt: 'Logo Dartcom',
+        width: 200,
+        height: 100,
+    },
+    {
+        key: 'aws',
+        src: '/aws.svg',
+        alt: 'Logo AWS',
+        width: 100,
+        height: 100,
     },
     {
         key: 'climateai',
         src: '/climateai.png',
         alt: 'Logo Climate Ai',
+        width: 200,
+        height: 100,
     },
     {
         key: 'tec',
         src: '/tec.svg',
         alt: 'Logo Tec de Monterrey',
-    }
+        width: 200,
+        height: 100,
+    },
+
 ]
 
 export const CarouselLogos = () => {
     return (
-        <div className="space-y-8">
-            <p className="text-center font-semibold text-xl">Built software at</p>
+        <div className="space-y-24">
+            <p className="text-center font-semibold text-xl">Built software / deployed infrastructure at</p>
             <Carousel
+                opts={{
+                    loop: true,
+                }}
                 plugins={[
                     Autoplay({
                         delay: 2000,
@@ -50,7 +67,7 @@ export const CarouselLogos = () => {
                     {
                         logosArr.map((logo) => (
                             <CarouselItem key={logo.key} className="flex md:basis-1/2 lg:basis-1/3 items-center justify-center">
-                                <Image src={logo.src} alt={logo.alt} width={200} height={100} />
+                                <Image src={logo.src} alt={logo.alt} width={logo.width} height={logo.height} />
                             </CarouselItem>
                         ))
                     }
